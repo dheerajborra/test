1374230efd99f66589f2316f8a911b84623e1682
Dheeraj Borra <rayofhopedheeraj@gmail.com>
2013-12-08 18:12:48 -0600

    First java file

---
+++ LatestVersion.java
@@ -0,0 +1,22 @@
+package codes;
+
+public class LatestVersion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "1.2.3.4";
+		String str2 = "1.2.3.35";
+		
+		str1 = str1.replace(".","A");
+		str2 = str2.replace(".","A");
+		
+		int t = str1.compareTo(str2);
+		if(t == 0)
+			System.out.println("Both are same");
+		if(t < 0)
+			System.out.println("2 is latest");
+		if(t > 0)
+			System.out.println("1 is latest");
+	}
+
+}
